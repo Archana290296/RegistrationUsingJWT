1e96f4f96ea33d0af710bfd95695c4ff07be5697
Archana Khandekar <archanakhandekar290296@gmail.com>
2023-12-27 19:13:37 +0530

    initial commit

---
+++ src/main/java/com/registration/RegistrationUsingJwtApplication.java
@@ -0,0 +1,13 @@
+package com.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationUsingJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationUsingJwtApplication.class, args);
+	}
+
+}
